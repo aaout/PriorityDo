03d61dde1ef8355b87d8ca910f3b7d7593e338c3
aaout <tiktik_suke@outlook.jp>
2020-11-14 16:58:01 +0900

    update todo list

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 import todo
 from todo.models import Todo
+
+from .forms import *
+from .models import *
 
 
 # classで書き換えたい
 def index(request):
     todo = Todo.objects.all()
-    context = {'todo': todo}
+    form = TodoForm()
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        # if form.is_valid():
+            # print('valid ok')
+        form.save()
+        return redirect('/')
+
+    context = {'todo': todo, 'form':form}
     return render(request, 'todo/index.html', context)
