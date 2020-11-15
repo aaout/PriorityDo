7a19b61abe20681a6182c035ca4ac3ee51cab0ce
aaout <tiktik_suke@outlook.jp>
2020-11-15 13:12:53 +0900

    おｋ

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="list"),
     path('update/<str:pk>/', views.updateTodo, name="update_todo"),
-    path('delete/<str:pk>/', views.deleteTodo, name="delete")
+    path('delete/<str:pk>/', views.deleteTodo, name="delete"),
+    path('sort_importance', views.sort_importance, name="sort_importance"),
+    path('sort_motivation', views.sort_motivation, name="sort_motivation"),
+    path('sort_progress', views.sort_progress, name="sort_progress"),
 ]
